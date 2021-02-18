6d1334cd5cd5e4c063a268b2ab346536dd101ddb
Kyle Hill <kylehill@gmail.com>
2021-02-18 14:09:06 -0500

    Update deps, lots of a11y changes

--- containers/LandingPage/index.tsx
+++ containers/LandingPage/index.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import Card from "components/Card";
-import { CardColor } from "types/card";
+import { CardColor, CardData } from "types/card";
+import Head from "next/head";
 
 const LandingPage = () => {
+  const handleClick = (card: CardData): void => {
+    console.log(card);
+  };
+
   return (
     <div>
+      <Head>
+        <title>Title TKTKTKTK</title>
+      </Head>
       hey it's my page
-      <Card clickable={true} discardable={true} card={{ value: 10, color: CardColor.Red }} />
-      <Card clickable={true} discardable={true} card={{ value: 15, color: CardColor.Blue }} />
+      <Card
+        clickable={true}
+        discardable={true}
+        card={{ value: 10, color: CardColor.Red }}
+        onClick={() => handleClick({ value: 10, color: CardColor.Red })}
+      />
+      <Card
+        clickable={true}
+        discardable={true}
+        card={{ value: 15, color: CardColor.Blue }}
+        onClick={() => handleClick({ value: 15, color: CardColor.Blue })}
+      />
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import "../global.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { SSRProvider } from "react-aria";
 import { AppProps } from "next/app";
+
+import "../global.css";
+
+if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
+  const axe = require("@axe-core/react");
+  axe(React, ReactDOM, 1000, {});
+}
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  );
 }
